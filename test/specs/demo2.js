748262eca0d00f9228e7a230eeabf7ec0ba20d24
alincode <alin.code@gmail.com>
2016-08-24 20:44:29 +0800

    simple login

---
+++ test/specs/demo2.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+describe('demo 2', function() {
+  it('login failure', function() {
+    browser.url('http://demo.keystonejs.com/keystone/signin');
+    browser.element('[name=email]').setValue('demo@keystonejs.com');
+    browser.element('[name=password]').setValue('1111');
+    browser.click('.Button--primary');
+    browser.waitForExist('.Alert--danger');
+    const message = browser.getText('.Alert--danger');
+    assert.equal(message,
+      'The email and password you entered are not valid.');
+  });
+
+  it('login successful', function() {
+    browser.url('http://demo.keystonejs.com/keystone/signin');
+    browser.element('[name=email]').setValue('demo@keystonejs.com');
+    browser.element('[name=password]').setValue('demo');
+    browser.click('.Button--primary');
+    browser.waitForExist('[href="/keystone/signout"]');
+  });
+});
